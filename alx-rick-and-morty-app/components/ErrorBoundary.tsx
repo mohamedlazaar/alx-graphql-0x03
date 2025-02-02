9b5ec729eb7d2d4eb28cf545040079ca4cec8548
mohamed lazaar <lazzarmohamed10@gmail.com>
2025-02-02 11:56:00 +0100

    monitor and Log Errors

--- alx-rick-and-morty-app/components/ErrorBoundary.tsx
+++ alx-rick-and-morty-app/components/ErrorBoundary.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
+import * as Sentry from "@sentry/react"
 
 interface State {
     hasError: boolean;
@@ -21,7 +21,7 @@ interface State {
     }
   
     componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
-      console.log({ error, errorInfo });
+      Sentry.captureException(error, { extra: errorInfo });
     }
   
     render() {
